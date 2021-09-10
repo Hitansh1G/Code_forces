ba62aaed99e5f67292c3406835d33fcba60b2a09
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-09-10 15:28:59 +0530

    Nearly Lucky Number

---
+++ Stones_on_the_Table.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+    for(int i = 1 ; i < n ; i++){
+        if( s[i-1] == s[i] ){
+            if(s[i] == 'R')
+                red = red + 1;
+            else if ( s[i] == 'G'){
+                green = green + 1;
+            }
+            else if(s[i]=='B')
+                blue = blue + 1;
+        }
+    }
+    int sum = red + green + blue;
+    cout << sum ; 
+  	return 0;
+}
