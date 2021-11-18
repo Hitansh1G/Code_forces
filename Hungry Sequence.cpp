4f81b6ee832b04d94b24c4090d5acc183d2a56cf
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-18 20:16:53 +0530

    Hungry Sequence

---
+++ Hungry Sequence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#define ll long long
+using namespace std;
+
+int main(){ 
+    int n, i; 
+    cin >> n; 
+    for( i=0 ; i<n ; i++)
+        cout << i + n * 10 <<" "; 
+        return 0;
+}
