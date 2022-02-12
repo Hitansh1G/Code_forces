65537c4806736d74b6e557b6a06b89520b36f438
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-12 21:24:00 +0530

    K-string

---
+++ k-String.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main(){
+    Fast
+    int k;
+    cin >> k;
+    string s;
+    char ch;
+    cin >>s;
+    int l=s.size(),count=0;
+    sort(s.begin(),s.begin()+s.size());
+    for(int i=0;i<s.size();i++){
+	    if(i%k==0){
+	       ch=s[i]; 
+	    }
+	    if(s[i]==ch){
+	        count++;
+	    }
+	}
+	if(l==count && count%k==0){
+	    for(int i=0;i<k;i++){
+	        for(int l=0;l<s.size();l+=k){
+	            cout<<s[l];
+	        }
+	    }
+	}else{
+	    cout<<"-1";
+	}
+    return 0;
+}
