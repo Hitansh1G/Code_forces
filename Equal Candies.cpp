fff57d2f92120f9cb5dc4d3cea886bae053f8ea7
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-10 20:29:28 +0530

    Equal Candies

--- Equal Candies.cpp
+++ Equal Candies.cpp
@@ -20,7 +20,18 @@ int main(){
     int t;
     cin >> t; 
     while(t--){
-        
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0 ; i<n ; i++){
+            cin >> arr[i];
+        }
+        int count=0;
+        int minimum = *min_element (arr, arr+n);
+        for(int i=0 ; i<n ; i++){
+            count += arr[i]-minimum;
+        }
+        cout<<count<<endl;
     }
     return 0;
 }
