cd87c2eb150ee8caa6562a03087c0c536ba31b34
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-13 11:12:37 +0530

    Promo

--- Promo.cpp
+++ Promo.cpp
@@ -19,15 +19,15 @@ int32_t main(){
     Fast
     
     
-    int n,q;
-    cin >> n >> q;
-    // int arr[n]
-    vector<int> arr(n);
-    for(int i=0 ; i<n ; i++){
-        cin >> arr[i];
-    }
-    // sort(arr,arr+n);
-    sort(arr.begin(), arr.end());
+    // int n,q;
+    // cin >> n >> q;
+    // // int arr[n]
+    // vector<int> arr(n);
+    // for(int i=0 ; i<n ; i++){
+    //     cin >> arr[i];
+    // }
+    // // sort(arr,arr+n);
+    // sort(arr.begin(), arr.end());
     // vector<int>largest;
     // vector<int>smallest;
    
@@ -55,24 +55,21 @@ int32_t main(){
     //     cout<<small<<endl;
     // }
         
-        vector <int> temp(n,arr[0]);
-        for(int i=1 ; i<n ; i++){
-            temp[i]=temp[i-1]+arr[i];
-        }
-
-        for(int i=0 ; i<q ; i++){
-            int x,y;
-            cin >> x >> y;
-            if(x == n){
-                cout << temp[y-1] << endl;
-            }
-            else{
-                int diff = x-y;
-                int flag = n-x;
-                int p = flag + y-1;
-                int sum = temp[p]-temp[flag-1];
-                cout << sum << endl;
-            }
-        }
-    return 0;
+        int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  vector<long long> pref(n + 1);
+  for (int i = 0; i < n; i++) {
+    pref[i + 1] = pref[i] + a[i];
+  }
+  while (q--) {
+    int x, y;
+    cin >> x >> y;
+    cout << pref[n - (x - y)] - pref[n - x] << '\n';
+  }
+  return 0;
 }
