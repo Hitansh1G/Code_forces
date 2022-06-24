702517d7e86f970f5f74d3a06ac468292aff8bf4
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-24 20:57:11 +0530

    Buttons

---
+++ Buttons.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int32_t main(){
+    Fast
+    ll n;
+    cin>>n;
+    cout << (n - 1) * n * (n + 1) / 6 + n << endl;
+    return 0;
+}
