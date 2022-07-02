8894e57581ae8f56f6bd49e8888734d00c6b7ba9
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-02 15:50:29 +0530

    Lovely Palindromes

---
+++ Lovely Palindromes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int32_t main(){
+    Fast
+    string a;
+    cin >> a;
+    for (int i=a.size()-1 ; i>=0 ; i--) {
+        a += a[i];
+    }
+    cout << a;
+    
+    return 0;
+}
