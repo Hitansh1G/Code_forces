c1d65a62f6ba524330924d0b59d93fe6e2345866
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-04 13:33:19 +0530

    Lucky Numbers

---
+++ Lucky Numbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int32_t main(){
+    Fast
+    int n;
+    cin >> n ;
+    //ans is 2^n+1
+    cout<<(2LL << n) - 2;
+    return 0;
+}
