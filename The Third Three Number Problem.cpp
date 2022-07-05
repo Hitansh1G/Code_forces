665db7ed731fc249031a5096e811ac4ead9b16b9
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-05 13:53:00 +0530

    The Third Three Number Problem

---
+++ The Third Three Number Problem.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int32_t main(){
+    Fast
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n%2==1) cout<<-1<<endl;
+        else
+        cout<<0<<" "<<n/2<<" "<<n/2<<endl;
+    }
+    return 0;
+}
