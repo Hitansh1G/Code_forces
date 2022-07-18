035a569d3190e65e271e2236cf06c58bfd285d42
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-18 18:17:20 +0530

    Theatre Square

---
+++ Theatre Square.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int32_t main(){
+    Fast
+    int n,m,a;
+    cin >> n >> m >> a;
+
+    int len = n/a;
+    if(n%a!=0) len++;
+    int bre = m/a;
+    if(m%a!=0) bre++;
+    
+    cout<<len*bre<<endl;
+
+    return 0;
+}
